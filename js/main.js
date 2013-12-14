fbbdfaee1ac97aad52c68c0ae8e285415a3fbb82
Boris Cherny <bcherny@gmail.com>
2013-12-14 15:05:14 -0800

    begin rendering

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
-/* global $, define, require, exports, module */
+/* global $, _, define, require, exports, module */
 ;(function(){
 
 	"use strict";
 
 	var config = {
+		element: document.body,
 		url: 'data.json'
 	};
 
+	var template = {
+
+		topic: function (data) {
+			console.log (data);
+
+			return ''
+		},
+
+		tree: function (html) {
+
+			return '<ul class="tree">' + html + '</ul>';
+
+		}
+
+	};
+
 	function process (data) {
+
+		var html = template.tree(
+			_.reduce(data.topics, function (accumulator, datum) {
+				return accumulator += template.topic(datum);
+			}, '')
+		);
+
+		config.element.innerHTML += html;
+
+		console.log (html);
 
 	}
 
