ffdd3574a5d920014a1c420fe5245edc75819a84
Boris Cherny <bcherny@gmail.com>
2013-12-14 14:56:08 -0800

    fetch data

---
+++ README.md
@@ -0,0 +1,6 @@
+wikimedia demo
+==============
+
+## Notes
+
+- I didn't fetch "http://unicorn.wmflabs.org/techtask/discussion.json" directly because of CORS restrictions. A more robust solution would be to use another, CORS-enabled server as a proxy, and request the JSON from there instead.
--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-define(function(require, exports, module) {
+/* global $, define, require, exports, module */
+;(function(){
+
+	"use strict";
+
+	var config = {
+		url: 'data.json'
+	};
 
 
-	
+	// fetch data
+	$.ajax({
+		url: config.url
+	})
+	.done(function(data) {
+		console.log ('data', data);
+	})
+	.fail(function(err) {
+		console.error(err);
+	});
 
-	
-});
+
+})();
